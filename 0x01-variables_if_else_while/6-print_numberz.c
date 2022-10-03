3580b95a3bcccfd44419f620aeb1c15e9c5708b3
Mbali Khumalo <shezi7314@gmail.com>
2022-10-03 11:33:53 -0700

    task 7

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numberz
+ *
+ * Description: print single digit numberz
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
+
