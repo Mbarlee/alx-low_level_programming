88fe3bde56a02c43f85d5782ac457927f39cb3e6
Mbali Khumalo <shezi7314@gmail.com>
2022-10-06 04:26:12 -0700

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,15 @@
 #include "main.h"
-
+#include <string.h>
 /**
-* cap_string -> capitalization function
-* @x: string param
-* Return: capitalized version of the string
+* _strcmp -> for comparing purpose
+* @s1: param1
+* @s2: param2
+* Return: integer
 */
-char *cap_string(char *x)
+int _strcmp(char *s1, char *s2)
 {
-	char spc[] = {32, 9, '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-	int len = 13;
-	int a = 0, i;
+	int result;
 
-	while (x[a])
-	{
-		i = 0;
-		while (i < len)
-		{
-			if ((a == 0 || x[a - 1] == spc[i]) && (x[a] >= 97 && x[a] <= 122))
-				x[a] = x[a] - 32;
-			i++;
-		}
-		a++;
-	}
-	return (x);
+	result = strcmp(s1, s2);
+	return (result);
 }
