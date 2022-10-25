f2558596b97b24ccf911ec884ee0d682ef9e8c56
Mbali <shezi7314@gmail.com>
2022-10-25 14:31:20 -0700

    2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -19,7 +19,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 		new_node->next = *head;
 	}
 	else
-		return(NULL);
+		return (NULL);
 	if (*head != NULL)
 		new_node->next = *head;
 	*head = new_node;
