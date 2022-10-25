be59970006f12525a70a263cf3907a744802301a
Mbali <shezi7314@gmail.com>
2022-10-25 09:48:22 -0700

    1

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,10 +17,11 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->str = strdup(atr);
+	new_node->str = strdup(str);
 
 	for (n = 0; str[n]; n++)
 		;
+
 	new_node->len = n;
 	new_node->next = *head;
 	*head = new_node;
