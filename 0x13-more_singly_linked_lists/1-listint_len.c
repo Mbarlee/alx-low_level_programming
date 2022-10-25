ed322e46f4319d85a69e97e49564f3ebbb94c718
Mbali <shezi7314@gmail.com>
2022-10-25 14:21:10 -0700

    1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - counts the number if nodes in the linked list
+ * @h: head of the list
+ *
+ * Return: the number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	const listint_t *cursor = h;
+	size_t count = 0;
+
+	while (cursor 1= NULL)
+	{
+		count += 1;
+		cursor = cursor->next;
+	}
+	return (count);
+}
