734a89ae9fa7c744e0667c5cc2852fafba5c66e5
Mbali <shezi7314@gmail.com>
2022-11-21 11:28:52 -0800

    1

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,26 +1,30 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
+ * rot13 - encodes a string in rot13
+ * @s: string to be encoded
  *
- * @n: integer params
- *
- * Return: 0
+ * Return: the resulting string
  */
 
-void print_number(int n)
+char *rot13(char *s)
 {
-	unsigned int x;
+	int i, j;
 
-	x = n;
-	if (n < 0)
+	char a[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char b[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		_putchar('-');
-		x = -n;
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+				break;
+			}
+		}
 	}
-	if (x / 10 != 0)
-	{
-		print_number(x / 10);
-	}
-	_putchar((x % 10) + '0');
+
+	return (s);
 }
